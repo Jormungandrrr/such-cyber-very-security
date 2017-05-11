e4b144dce90494850035b6b197ae96d197c57c44
PetervBijsterveldt <petervbijsterveldt@gmail.com>
2017-05-11 12:54:39 +0200

    Maven/Spring implementation

---
+++ src/main/java/snorter/config/WebConfig.java
@@ -0,0 +1,29 @@
+package snorter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("snorter")
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+}
---
+++ src/main/java/snorter/controllers/MainController.java
@@ -0,0 +1,9 @@
+package snorter.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+
+}
