db1f476120bfac596856a812d93c6b2d60efe438
PetervBijsterveldt <petervbijsterveldt@gmail.com>
2017-06-14 15:57:59 +0200

    Added SnortWebsocketServer

---
+++ snortWebsocketServer/src/main/java/Core.java
@@ -0,0 +1,76 @@
+import org.json.JSONObject;
+import peter.networking.websockets.WebSocketClientEventHandler;
+import peter.networking.websockets.WebsocketServer;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Core implements WebSocketClientEventHandler {
+
+    public Core() {
+        try {
+            WebsocketServer server = new WebsocketServer(8080, this);
+            Runtime runtime = Runtime.getRuntime();
+            Process terminal = runtime.exec("tail -f /var/log/snort/alert.csv");
+
+            InputStream in = terminal.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            File dataFile = new File("/alertHistory.csv");
+
+            if (!dataFile.exists())
+                dataFile.createNewFile();
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(dataFile, true));
+
+            String output;
+
+            while ((output = reader.readLine()) != null) {
+                //[0] = timestamp; [1] = message; [2] = source IP
+                String[] data = output.split(",");
+
+                //In case of the alert message containing ','.
+                String combinedMessage = "";
+                for (int i = 1; i < data.length - 1; i++)
+                    combinedMessage += data[i];
+
+                String[] correctData = {
+                        data[0],
+                        combinedMessage,
+                        data[2]
+                };
+
+                for (int i = 0; i < correctData.length; i++)
+                    correctData[i] = correctData[i].trim();
+
+                JSONObject json = new JSONObject();
+
+                json.put("Hour", correctData[0].substring(6, correctData[0].indexOf(':')));
+                json.put("Type", correctData[1]);
+
+                //Only lookup a location if the source IP is not a local IP address.
+                if (correctData[2].split(".")[0].equals("192"))
+                    json.put("Location", "Internal");
+                else {
+                    GeoLocation location = GeoIPv4.getLocation(correctData[2]);
+
+                    json.put("Longitude", location.getLongitude());
+                    json.put("Latitude", location.getLatitude());
+                    json.put("CountryCode", location.getCountryCode());
+                    json.put("CountryName", location.getCountryName());
+                }
+
+                System.out.println(json.toString());
+                writer.write(json.toString() + '\n');
+                writer.flush();
+                server.messageAllClients(json.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onMessage(Socket socket, String s) {
+        System.out.println(s);
+    }
+}
---
+++ snortWebsocketServer/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Core core = new Core();
+    }
+}
