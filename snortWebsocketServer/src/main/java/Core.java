2e55e23e8fe868e436a3ae7e1af6d3c50c2659ac
PetervBijsterveldt <petervbijsterveldt@gmail.com>
2017-06-15 15:10:42 +0200

    Added IP pattern matcher

--- snortWebsocketServer/src/main/java/Core.java
+++ snortWebsocketServer/src/main/java/Core.java
@@ -2,13 +2,21 @@ import org.json.JSONObject;
 import peter.networking.websockets.WebSocketClientEventHandler;
 import peter.networking.websockets.WebsocketServer;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.regex.Pattern;
 
 public class Core implements WebSocketClientEventHandler {
 
     public Core() {
         try {
+            final String IPADDRESS_PATTERN =
+                    "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+
+            Pattern pattern = Pattern.compile(IPADDRESS_PATTERN);
+
             WebsocketServer server = new WebsocketServer(8080, this);
             Runtime runtime = Runtime.getRuntime();
             Process terminal = runtime.exec("tail -f /var/log/snort/alert.csv");
@@ -40,16 +48,21 @@ public class Core implements WebSocketClientEventHandler {
                 json.put("Hour", correctData[0].substring(6, correctData[0].indexOf(':')));
                 json.put("Type", correctData[1]);
 
-                //Only lookup a location if the source IP is not a local IP address.
-                if (correctData[2].substring(0, correctData[2].indexOf(".")).equals("192"))
-                    json.put("Location", "Internal");
-                else {
-                    GeoLocation location = GeoIPv4.getLocation(correctData[2]);
+                if (pattern.matcher(correctData[2]).find()) {
+                    //Only lookup a location if the source IP is not a local IP address.
+                    if (correctData[2].substring(0, correctData[2].indexOf(".")).equals("192"))
+                        json.put("Location", "Internal");
+                    else {
+                        GeoLocation location = GeoIPv4.getLocation(correctData[2]);
 
-                    json.put("Longitude", location.getLongitude());
-                    json.put("Latitude", location.getLatitude());
-                    json.put("CountryCode", location.getCountryCode());
-                    json.put("CountryName", location.getCountryName());
+                        json.put("Longitude", location.getLongitude());
+                        json.put("Latitude", location.getLatitude());
+                        json.put("CountryCode", location.getCountryCode());
+                        json.put("CountryName", location.getCountryName());
+                        json.put("ContainsIP", "true");
+                    }
+                } else {
+                    json.put("ContainsIP", "false");
                 }
 
                 System.out.println(json.toString());
