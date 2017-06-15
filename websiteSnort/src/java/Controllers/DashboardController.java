b27243fa078c0d830f2cdc77f50682ffc24e358b
Redxice <m.boeters@student.fontys.nl>
2017-06-15 13:03:21 +0200

    daDSAF

--- websiteSnort/src/java/Controllers/DashboardController.java
+++ websiteSnort/src/java/Controllers/DashboardController.java
@@ -5,14 +5,15 @@
  */
 package Controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.ArrayList;
+import Models.GeoIPv4;
+import Models.GeoLocation;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,12 +26,57 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/dashboard")
 public class DashboardController {
 
-    @RequestMapping("/dashboard")
-    public String dashboard(Model model, HttpServletRequest request) {
-        
-            HashMap<String,Integer> list = new HashMap<>();
-            list.put("'testkey'",1);
-            request.setAttribute("list", list);
-            return "dashboard";
+    @RequestMapping()
+    public String home(Model model, HttpServletRequest request) {
+        GeoLocation location = GeoIPv4.getLocation("101.52.36.93");
+
+        HashMap<String, Integer> list = new HashMap<>();
+
+        list.put("'testkey'", 1);
+        request.setAttribute("list", list);
+        return "dashboard";
+    }
+
+    private String alertFile() throws IOException {
+        File file = new File("/var/log/snort/alert.csv");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            String[] data = line.split(",");
+
+            //In case of the alert message containing ','.
+            String combinedMessage = "";
+            for (int i = 1; i < data.length - 1; i++) {
+                combinedMessage += data[i];
+            }
+
+            String[] correctData = {
+                data[0],
+                combinedMessage,
+                data[2]
+            };
+            
+            for (int i = 0; i < correctData.length; i++) {
+                correctData[i] = correctData[i].trim();
+            }
+
+            JSONObject json = new JSONObject();
+
+            json.put("Hour", correctData[0].substring(6, correctData[0].indexOf(':')));
+            json.put("Type", correctData[1]);
+
+            //Only lookup a location if the source IP is not a local IP address.
+            if (correctData[2].split(".")[0].equals("192")) {
+                json.put("Location", "Internal");
+            } else {
+                GeoLocation location = GeoIPv4.getLocation(correctData[2]);
+
+                json.put("Longitude", location.getLongitude());
+                json.put("Latitude", location.getLatitude());
+                json.put("CountryName", location.getCountryName());
+            }
+        }
+        return "";
     }
 }
